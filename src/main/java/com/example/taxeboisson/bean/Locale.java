fa158066673153633e4f7ace8c7e86d037e1a571
Hp <mohamed.kiddi.1@gmail.com>
2022-04-09 04:11:22 +0000

    fix some problem in WS links

--- src/main/java/com/example/taxeboisson/bean/Locale.java
+++ src/main/java/com/example/taxeboisson/bean/Locale.java
@@ -1,16 +1,13 @@
 package com.example.taxeboisson.bean;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.ManyToOne;
-import javax.persistence.OneToOne;
+import javax.persistence.*;
+import java.util.List;
 
 @Entity
 public class Locale {
 
-    @Id @GeneratedValue(strategy = GenerationType.AUTO)
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
     private String ref;
     private String adresse;
@@ -26,39 +23,52 @@ public class Locale {
     private Secteur secteur;
 
 
+
+
     public String getRef() {
         return ref;
     }
+
     public void setRef(String ref) {
         this.ref = ref;
     }
+
     public String getAdresse() {
         return adresse;
     }
+
     public void setAdresse(String adresse) {
         this.adresse = adresse;
     }
+
     public String getRue() {
         return rue;
     }
+
     public void setRue(String rue) {
         this.rue = rue;
     }
+
     public Redevable getRedevable() {
         return redevable;
     }
+
     public void setRedevable(Redevable redevable) {
         this.redevable = redevable;
     }
+
     public CategorieLocale getCategorieLocale() {
         return categorieLocale;
     }
+
     public void setCategorieLocale(CategorieLocale categorieLocale) {
         this.categorieLocale = categorieLocale;
     }
+
     public Secteur getSecteur() {
         return secteur;
     }
+
     public void setSecteur(Secteur secteur) {
         this.secteur = secteur;
     }
